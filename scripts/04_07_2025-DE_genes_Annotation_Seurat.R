275f98ab73b6d4073ae0324edb61fc276e9cef55
srivalli.kolla <kollasrivalli3@gmail.com>
2025-07-05 14:20:06 +0200

    Updated scripts for graphs

--- scripts/04_07_2025-DE_genes_Annotation_Seurat.R
+++ scripts/04_07_2025-DE_genes_Annotation_Seurat.R
@@ -1,11 +1,12 @@
 library(Seurat)
 library(dplyr)
 library(ggplot2)
+library(SeuratDisk)
 
-#### Load processed object ####
+#### Load processed integrated object ####
 integrated <- readRDS("/home/gruengroup/srivalli/Github/ACM_SN_R_2025/data/integrated_processed.rds")
 
-#### DE analysis ####
+#### Perform DE analysis ####
 markers <- FindAllMarkers(
   integrated,
   only.pos = TRUE,
@@ -17,12 +18,70 @@ markers <- FindAllMarkers(
 top5 <- markers %>% group_by(cluster) %>% top_n(5, avg_log2FC)
 top10 <- markers %>% group_by(cluster) %>% top_n(10, avg_log2FC)
 
+# Save DE marker results
 write.csv(markers, "/home/gruengroup/srivalli/Github/ACM_SN_R_2025/data/de_all_markers.csv", row.names = FALSE)
 write.csv(top10, "/home/gruengroup/srivalli/Github/ACM_SN_R_2025/data/de_top_10_clusters.csv", row.names = FALSE)
+saveRDS(markers, file = "/home/gruengroup/srivalli/Github/ACM_SN_R_2025/data/de_all_markers.rds")
 
-# Dot plot for top 5
+# Dot plot for top 5 marker genes
 p_dot <- DotPlot(integrated, features = unique(top5$gene)) + RotatedAxis()
 ggsave("/home/gruengroup/srivalli/Github/ACM_SN_R_2025/data/dotplot_top5_markers.png", p_dot, width = 10, height = 6)
 
+#### Manually set human-readable cluster names ####
+cluster_names <- c(
+  "Fibroblasts",                        # 0
+  "Endothelial/Progenitor Cells",      # 1
+  "Cardiomyocytes (ventricular)",      # 2
+  "Mast Cells",                         # 3
+  "Epicardial/Endothelial Cells",      # 4
+  "Cardiomyocytes (possibly atrial)",  # 5
+  "Pericytes/Vascular SMC",            # 6
+  "Lymphatic Endothelial Cells",       # 7
+  "Endocardial/Conduction Cells",      # 8
+  "Immune Cells (Macrophages)",        # 9
+  "Pericytes/Fibroblasts",             # 10
+  "B Lymphocytes",                     # 11
+  "T Lymphocytes",                     # 12
+  "Vascular Smooth Muscle Cells",     # 13
+  "Proliferating/Cycling Cells",      # 14
+  "Neural Crest/Schwann Cells",       # 15
+  "Epicardial/Mesothelial Cells"      # 16
+)
 
-saveRDS(markers, file = "/home/gruengroup/srivalli/Github/ACM_SN_R_2025/data/de_all_markers.rds")
+# Map readable cluster names
+integrated$cluster_named <- factor(
+  cluster_names[as.numeric(as.character(integrated$seurat_clusters)) + 1],
+  levels = cluster_names
+)
+
+#### UMAP Plots ####
+dir.create("/home/gruengroup/srivalli/Github/ACM_SN_R_2025/plots", showWarnings = FALSE)
+
+# 1. Seurat Cluster ID
+p0 <- DimPlot(integrated, group.by = "seurat_clusters", label = TRUE, repel = TRUE) +
+  ggtitle("UMAP by Seurat Cluster ID")
+ggsave("/home/gruengroup/srivalli/Github/ACM_SN_R_2025/plots/umap_seurat_clusters.png", p0, width = 10, height = 7)
+
+# 2. Named clusters
+p1 <- DimPlot(integrated, group.by = "cluster_named", label = TRUE, repel = TRUE) +
+  ggtitle("UMAP by Annotated Cluster")
+ggsave("/home/gruengroup/srivalli/Github/ACM_SN_R_2025/plots/umap_named_clusters.png", p1, width = 10, height = 7)
+
+# 3. Sample-wise UMAP
+if ("sample" %in% colnames(integrated@meta.data)) {
+  p2 <- DimPlot(integrated, group.by = "sample", label = FALSE) +
+    ggtitle("UMAP by Sample")
+  ggsave("/home/gruengroup/srivalli/Github/ACM_SN_R_2025/plots/umap_by_sample.png", p2, width = 10, height = 7)
+}
+
+# 4. Genotype-wise UMAP
+if ("genotype" %in% colnames(integrated@meta.data)) {
+  p3 <- DimPlot(integrated, group.by = "genotype", label = FALSE) +
+    ggtitle("UMAP by Genotype")
+  ggsave("/home/gruengroup/srivalli/Github/ACM_SN_R_2025/plots/umap_by_genotype.png", p3, width = 10, height = 7)
+}
+
+#### Save annotated Seurat object ####
+saveRDS(integrated, "/home/gruengroup/srivalli/Github/ACM_SN_R_2025/data/integrated_named_clusters.rds")
+SaveH5Seurat(integrated, filename = "/home/gruengroup/srivalli/Github/ACM_SN_R_2025/data/integrated_named_clusters.h5Seurat", overwrite = TRUE)
+Convert("/home/gruengroup/srivalli/Github/ACM_SN_R_2025/data/integrated_named_clusters.h5Seurat", dest = "h5ad", overwrite = TRUE)
--- scripts/04_07_2025-Marker_genes_Annotation_Seurat.R
+++ scripts/04_07_2025-Marker_genes_Annotation_Seurat.R
@@ -1,75 +1,128 @@
 library(Seurat)
 library(dplyr)
-library(plyr)
 library(ggplot2)
 library(SeuratDisk)
 
-#### Load integrated object ####
+#-------------------------#
+# Load Data
+#-------------------------#
 integrated <- readRDS("/home/gruengroup/srivalli/Github/ACM_SN_R_2025/data/integrated_processed.rds")
 
-#### Load marker gene reference ####
-marker_ref <- read.csv("/home/gruengroup/srivalli/Github/ACM_SN_R_2025/data/marker_genes_heart_copy - marker_genes.csv", stringsAsFactors = FALSE)
+marker_ref <- read.csv("/home/gruengroup/srivalli/Github/ACM_SN_R_2025/data/marker_genes_heart.csv", stringsAsFactors = FALSE)
+colnames(marker_ref)[1:3] <- c("Cell.Type", "Subtype", "Marker.Genes")
 marker_ref$Marker.Genes <- toupper(marker_ref$Marker.Genes)
 marker_ref$marker_list <- strsplit(marker_ref$Marker.Genes, "[ ,;]+")
 
-#### Load DEGs or use top N from scratch ####
-markers <- readRDS("/home/gruengroup/srivalli/Github/ACM_SN_R_2025/data/de_all_markers.rds")
-top_markers <- markers %>% group_by(cluster) %>% top_n(30, avg_log2FC)
-top_markers$gene <- toupper(top_markers$gene)
+#-------------------------#
+# Compute Avg Expression per Cluster
+#-------------------------#
+DefaultAssay(integrated) <- "RNA"
+integrated <- NormalizeData(integrated)
+avg_exp <- AverageExpression(integrated, return.seurat = TRUE, assays = "RNA", slot = "data")
+avg_matrix <- GetAssayData(avg_exp, assay = "RNA", slot = "data")
+clusters <- levels(integrated$seurat_clusters)
 
-#### Matching markers to annotation ####
+#-------------------------#
+# Scoring: Top 2 Matching Cell Types per Cluster
+#-------------------------#
 annotated_clusters <- data.frame(
-  cluster = unique(top_markers$cluster),
-  CellType_Subtype = NA,
-  BroadType = NA
+  cluster = clusters,
+  CellType_Subtype_1 = NA,
+  BroadType_1 = NA,
+  Score_1 = NA,
+  CellType_Subtype_2 = NA,
+  BroadType_2 = NA,
+  Score_2 = NA
 )
 
-for (i in seq_along(annotated_clusters$cluster)) {
-  clust <- annotated_clusters$cluster[i]
-  clust_genes <- unique(top_markers$gene[top_markers$cluster == clust])
-  best_match <- NULL
-  best_score <- 0
-  for (j in 1:nrow(marker_ref)) {
-    ref_genes <- marker_ref$marker_list[[j]]
-    score <- length(intersect(clust_genes, ref_genes))
-    if (score > best_score) {
-      best_score <- score
-      best_match <- j
-    }
-  }
-  if (!is.null(best_match)) {
-    annotated_clusters$CellType_Subtype[i] <- paste0(
-      marker_ref$Cell.Type[best_match], "_", marker_ref$Subtype...State[best_match]
-    )
-    annotated_clusters$BroadType[i] <- marker_ref$Cell.Type[best_match]
-  }
+for (clust in clusters) {
+  gene_expr <- avg_matrix[, clust]
+
+  # Mean expression over marker genes
+  scores <- sapply(seq_len(nrow(marker_ref)), function(j) {
+    ref_genes <- intersect(marker_ref$marker_list[[j]], names(gene_expr))
+    if (length(ref_genes) == 0) return(0)
+    mean(gene_expr[ref_genes], na.rm = TRUE)
+  })
+
+  ranked <- order(scores, decreasing = TRUE)
+  top1 <- ranked[1]
+  top2 <- ranked[2]
+
+  # Assign top hits
+  annotated_clusters[annotated_clusters$cluster == clust, ] <- list(
+    clust,
+    paste0(marker_ref$Cell.Type[top1], "_", marker_ref$Subtype[top1]),
+    marker_ref$Cell.Type[top1],
+    scores[top1],
+    paste0(marker_ref$Cell.Type[top2], "_", marker_ref$Subtype[top2]),
+    marker_ref$Cell.Type[top2],
+    scores[top2]
+  )
 }
 
-# Map annotations
-ct_map <- setNames(annotated_clusters$CellType_Subtype, annotated_clusters$cluster)
-broad_map <- setNames(annotated_clusters$BroadType, annotated_clusters$cluster)
+#-------------------------#
+# Add Top Matches to Seurat Object
+#-------------------------#
+map1 <- setNames(annotated_clusters$CellType_Subtype_1, annotated_clusters$cluster)
+map2 <- setNames(annotated_clusters$CellType_Subtype_2, annotated_clusters$cluster)
 
-integrated$marker_subtype <- plyr::mapvalues(
-  as.character(integrated$seurat_clusters),
-  names(ct_map),
-  ct_map
+integrated$marker_subtype_top1 <- dplyr::recode(as.character(integrated$seurat_clusters), !!!map1)
+integrated$marker_subtype_top2 <- dplyr::recode(as.character(integrated$seurat_clusters), !!!map2)
+
+#-------------------------#
+# Create Plot Output Directory
+#-------------------------#
+#dir.create("/home/gruengroup/srivalli/Github/ACM_SN_R_2025/plots", showWarnings = FALSE)
+
+#-------------------------#
+# UMAP Plots
+#-------------------------#
+# 1. Seurat Clusters
+p0 <- DimPlot(integrated, group.by = "seurat_clusters", label = TRUE, repel = TRUE) +
+  ggtitle("UMAP: Seurat Clusters")
+ggsave("/home/gruengroup/srivalli/Github/ACM_SN_R_2025/plots/umap_seurat_clusters.png", p0, width = 10, height = 7)
+
+# 2. Marker Top 1
+p1 <- DimPlot(integrated, group.by = "marker_subtype_top1", label = TRUE, repel = TRUE) +
+  ggtitle("UMAP: Marker Gene Subtype (Top 1 Match)")
+ggsave("/home/gruengroup/srivalli/Github/ACM_SN_R_2025/plots/umap_marker_subtype_top1.png", p1, width = 10, height = 7)
+
+# 3. Marker Top 2
+p2 <- DimPlot(integrated, group.by = "marker_subtype_top2", label = TRUE, repel = TRUE) +
+  ggtitle("UMAP: Marker Gene Subtype (Top 2 Match)")
+ggsave("/home/gruengroup/srivalli/Github/ACM_SN_R_2025/plots/umap_marker_subtype_top2.png", p2, width = 10, height = 7)
+
+# 4. Sample ID
+if ("sample_id" %in% colnames(integrated@meta.data)) {
+  p3 <- DimPlot(integrated, group.by = "sample_id", label = TRUE, repel = TRUE) +
+    ggtitle("UMAP: Sample ID")
+  ggsave("/home/gruengroup/srivalli/Github/ACM_SN_R_2025/plots/umap_sample_id.png", p3, width = 10, height = 7)
+}
+
+# 5. Genotype
+if ("genotype" %in% colnames(integrated@meta.data)) {
+  p4 <- DimPlot(integrated, group.by = "genotype", label = TRUE, repel = TRUE) +
+    ggtitle("UMAP: Genotype")
+  ggsave("/home/gruengroup/srivalli/Github/ACM_SN_R_2025/plots/umap_genotype.png", p4, width = 10, height = 7)
+}
+
+#-------------------------#
+# Save Annotated Seurat Object
+#-------------------------#
+saveRDS(integrated, "/home/gruengroup/srivalli/Github/ACM_SN_R_2025/data/integrated_marker_annotated.rds")
+
+SaveH5Seurat(integrated,
+  filename = "/home/gruengroup/srivalli/Github/ACM_SN_R_2025/data/integrated_marker_annotated.h5Seurat",
+  overwrite = TRUE
 )
-integrated$marker_broad <- plyr::mapvalues(
-  as.character(integrated$seurat_clusters),
-  names(broad_map),
-  broad_map
-)
 
-# UMAPs with annotations
-p1 <- DimPlot(integrated, group.by = "marker_subtype", label = TRUE, repel = TRUE) +
-  ggtitle("Clusters Annotated by CellType - Subtype")
-ggsave("/home/gruengroup/srivalli/Github/ACM_SN_R_2025/plots/umap_marker_subtype.png", p1, width = 10, height = 7)
+Convert("/home/gruengroup/srivalli/Github/ACM_SN_R_2025/data/integrated_marker_annotated.h5Seurat",
+        dest = "h5ad", overwrite = TRUE)
 
-p2 <- DimPlot(integrated, group.by = "marker_broad", label = TRUE, repel = TRUE) +
-  ggtitle("Clusters Annotated by Broad Cell Type")
-ggsave("/home/gruengroup/srivalli/Github/ACM_SN_R_2025/plots/umap_marker_broad.png", p2, width = 10, height = 7)
-
-# Save annotated version
-SaveH5Seurat(integrated, filename = "/home/gruengroup/srivalli/Github/ACM_SN_R_2025/plots/integrated_annotated.h5Seurat", overwrite = TRUE)
-Convert("/home/gruengroup/srivalli/Github/ACM_SN_R_2025/data/integrated_annotated.h5Seurat", dest = "h5ad", overwrite = TRUE)
-saveRDS(integrated, file = "/home/gruengroup/srivalli/Github/ACM_SN_R_2025/data/integrated_annotated.rds")
+#-------------------------#
+# Save Annotation Table
+#-------------------------#
+write.csv(annotated_clusters,
+          "/home/gruengroup/srivalli/Github/ACM_SN_R_2025/data/marker_gene_cluster_annotations.csv",
+          row.names = FALSE)
